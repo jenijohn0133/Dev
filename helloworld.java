37bbaab2d9b10f747c59ddc27fe7cc33ca5306af
jenijohn0133 <168919174+jenijohn0133@users.noreply.github.com>
2025-03-28 08:38:39 +0530

    helloworld.java

---
+++ helloworld.java
@@ -0,0 +1,12 @@
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // Calling the greet method
+        greet();
+    }
+
+    public static void greet() {
+        System.out.println("Hello, World! Welcome to Java programming.");
+    }
+}
